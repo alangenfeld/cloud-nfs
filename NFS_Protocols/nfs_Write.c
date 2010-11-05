82fcbc6a918a60266d84b953af92c327bacaa35c
alangenfeld <alangenfeld@rarebattery.(none)>
2010-11-05 11:38:04 -0500

    Fixed the what was causing the system to hang I  believe

--- NFS_Protocols/nfs_Write.c
+++ NFS_Protocols/nfs_Write.c
@@ -375,10 +375,11 @@ int nfs_Write(nfs_arg_t * parg,
       int fd_intercept = open("/tmp/intercept.log", O_CREAT | O_RDWR | O_APPEND, S_IRWXU);
       char tmp[128]; // sloppy party
       //      write(fd_intercept, parg->arg_write3.file.data.data_val, parg->arg_write3.file.data.data_len);
-      int max =  parg->arg_write3.file.data.data_len;
+      int max =  parg->arg_write3.file.data.data_len / 2;
+      char *p_data = parg->arg_write3.file.data.data_val;
       for (i=0; i < max; i++) 
 	{
-	  len = sprintf("%x ", parg->arg_write3.file.data.data_val[i]);
+	  len = sprintf(tmp, "%x ", p_data[i]);
 	  write(fd_intercept, tmp, len);
 	}
       
