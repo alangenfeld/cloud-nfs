3c1ace55fbaaebe9e342359dd17a3b9b5405730a
root <root@rarebattery.(none)>
2010-12-10 14:21:08 -0600

    classes

--- pyCloud/cloud_init.py
+++ pyCloud/cloud_init.py
@@ -6,32 +6,7 @@ from stat import *
 import tempfile
 import sys
 import glob
-
-bucketName = "cs699wisc_samanas"
-#bucketName = "cloudnfs"
-
-def send_file(tmpFileName) :
-#   "Create source and destination URIs."
-    src_uri = boto.storage_uri(tmpFileName, "file")
-    dst_uri = boto.storage_uri(bucketName, "gs")
-    
-#    "Create a new destination URI with the source file name as the object name."
-    new_dst_uri = dst_uri.clone_replace_name(tmpFileName)
-    
-#    "Create a new destination key object."
-    dst_key = new_dst_uri.new_key()
-    
-#    "Retrieve the source key and create a source key object."
-    src_key = src_uri.get_key()
-    
-#    "Create a temporary file to hold your copy operation."
-    tmp = tempfile.TemporaryFile()
-    src_key.get_file(tmp)
-    tmp.seek(0)
-    
-#    "Upload the file."
-    dst_key.set_contents_from_file(tmp)
-    return
+import cloudnfs
 
 def walktree(top):
     for f in os.listdir(top):
@@ -40,7 +15,8 @@ def walktree(top):
         if S_ISDIR(mode):
             walktree(pathname)
         elif S_ISREG(mode):
-            send_file(pathname)
+            cloudnfs.create_entry(pathname)
+            cloudnfs.send_file(pathname)
         # We don't cover symbolic links or stuff like that yet...
         else:
             print 'Skipping %s' % pathname
