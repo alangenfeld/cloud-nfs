581937443f18e8da1f4e111a6510d24ad070fe56
root <root@rarebattery.(none)>
2010-12-16 17:25:15 -0600

    Add working init thing with classes. Better print method.

--- pyCloud/cloud_init.py
+++ pyCloud/cloud_init.py
@@ -5,21 +5,21 @@ import os
 from stat import *
 import tempfile
 import sys
-import glob
 import cloudnfs
 
-def walktree(top):
+def walktree(top, meta_list):
     for f in os.listdir(top):
         pathname = os.path.join(top, f)
         mode = os.stat(pathname)[ST_MODE]
         if S_ISDIR(mode):
-            walktree(pathname)
+            walktree(pathname, meta_list)
         elif S_ISREG(mode):
-            cloudnfs.create_entry(pathname)
-            cloudnfs.send_file(pathname)
+            cloudnfs.send_file(pathname, pathname)
+            cloudnfs.create_entry(pathname, meta_list)
         # We don't cover symbolic links or stuff like that yet...
         else:
             print 'Skipping %s' % pathname
+    #cloudnfs.print_meta()
 
 ###############################################################################
 
@@ -33,4 +33,9 @@ mnt_point = sys.argv[1]
 # Change to given path in argv[1]..if it is indeed a path
 os.chdir(mnt_point)
 
-walktree(mnt_point)
+meta_list = cloudnfs.get_list()
+#print meta_list
+walktree(mnt_point, meta_list)
+#for m in meta_list:
+#    m.print_e()
+cloudnfs.store_list(meta_list)
--- pyCloud/cloudnfs.py
+++ pyCloud/cloudnfs.py
@@ -1,9 +1,10 @@
 import boto
 import os
 import tempfile
+import pickle
 
-bucketName = "cloudnfs"
-#bucketName = "cs699wisc_samanas"
+#bucketName = "cloudnfs"
+bucketName = "cs699wisc_samanas"
 
 def download(srcName, dstName) :
     "Download the files."
@@ -56,20 +57,57 @@ def send_file(srcName, dstName) :
     return
 
 class entry :
-    def __init__(name):
+    def __init__(self, name):
         self.name = name
         self.versions = 1
         self.version_map = dict({1:get_curr_bucket()})
+        self.active = True
+    # This assumes that different versions will always be in a different bucket
+    # Thus, versioning not supported yet.
     def update():
         self.versions += 1
         self.version_map[self.versions] = get_curr_bucket()
+    def __str__(self):
+        return self.name
+    def print_e(self):
+        print '%s; Version: %d' % (self.name, self.versions)
+        print self.active
+        print self.version_map
+
+def get_curr_bucket():
+    return bucketName
+
+def print_meta():
+    meta_list = get_list()
+    for e in meta_list :
+        print e
+
+def create_entry(name, entry_list):
+    entry_list.append(entry(name))
     
 
-def create_entry(name):
-    entry_list.append(entry(name))
+# Creates list if doesn't already exist. If it does, save it.
+def store_list(new_list):
+    meta_list = open('meta.pkl', 'wb')
+    pickle.dump(new_list, meta_list)
+    meta_list.close()
+    send_file('meta.pkl', 'meta.pkl')
 
-
-
-
-
-
+def get_list():
+    try:
+        # meta.pkl shall be stored in the cloud, we will attempt to download
+        download('meta.pkl', 'meta.pkl')
+    except boto.exception.InvalidUriError:
+        # If it isn't there, create locally and instantiate new list.
+        meta_list = open('meta.pkl', 'wb')
+        new_list = []
+    else:
+        # meta.pkl exists, lets load it and return.
+        meta_list = open('meta.pkl', 'rwb')
+        try:
+            new_list = pickle.load(meta_list)
+        except EOFError:
+            # The list we downloaded is empty. Return empty list.
+            new_list = []
+    meta_list.close()
+    return new_list
